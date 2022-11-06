059dca55ea723c812b4f7508d039e5e70917d485
Ricardo Magalhães <ricardo.magalhaes@wealthsimple.com>
2022-11-06 18:49:49 -0500

    Add auth and add links

--- graphql/context.ts
+++ graphql/context.ts
@@ -1,7 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import prisma from "../lib/prisma";
+import { Claims, getSession } from "@auth0/nextjs-auth0";
+
+// BEFORE adding auth stuff we only needed Prisma
 
 export type Context = {
+  user?: Claims;
+  accessToken?: string;
   prisma: PrismaClient;
 };
 
@@ -9,7 +14,19 @@ export type Context = {
 // to the Prisma Client and be able send queries to the database
 
 export async function createContext({ req, res }): Promise<Context> {
+  const session = getSession(req, res);
+
+  // if the user is not logged in, omit returning the user and accessToken
+  if (!session) return { prisma };
+
+  const { user, accessToken } = session;
   return {
+    user,
+    accessToken,
     prisma,
   };
 }
+
+// The getSession() function from Auth0 returns information about the logged-in
+// user and the access token.This data is then included in the GraphQL context.
+// Your queries and mutations can now access the authentication state.
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import "../styles/tailwind.css";
 import { ApolloProvider } from "@apollo/client";
+import { UserProvider } from "@auth0/nextjs-auth0";
 import apolloClient from "../lib/apollo";
 import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ApolloProvider client={apolloClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ApolloProvider>
+    <UserProvider>
+      <ApolloProvider client={apolloClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ApolloProvider>
+    </UserProvider>
   );
 }
 
